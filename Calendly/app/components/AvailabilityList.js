4f4c471200eec49cf05342330c00d6aefadfe54a
Mathew <mmacd97@gmail.com>
2017-11-19 23:33:56 -0800

    Made the availability list pretty (#35)
    * Made the availability list look prettier
    
    * Updated snapshots

--- Calendly/app/components/AvailabilityList.js
+++ Calendly/app/components/AvailabilityList.js
@@ -5,22 +5,33 @@ import PropTypes from 'prop-types';
 
 import TimeRow from './TimeRow';
 
+import * as STYLES from '../common/Styles';
+
 class AvailabilityList extends Component {
-  _renderSeparator = () => <View style={{ height: 10, backgroundColor: 'grey' }} />;
+  _renderSeparator = () => (
+    <View
+      style={{
+        height: 5,
+        backgroundColor: STYLES.COLOR_PRIMARY,
+      }}
+    />
+  );
 
   _renderItem = item => (
     <TimeRow
       text={moment(item.item)
-        .add(7, 'hours')
-        .format('LT')}
+        .add(1, 'month')
+        .utc()
+        .calendar()}
     />
   );
 
   render() {
     console.log(`isFetching is: ${this.props.isFetching}`);
     console.log(`times is: ${this.props.times}`);
+    console.log(`duration is: ${this.props.duration}`);
     return (
-      <View>
+      <View style={{ backgroundColor: STYLES.COLOR_PRIMARY }}>
         <FlatList
           data={this.props.times}
           ItemSeparatorComponent={this._renderSeparator}
