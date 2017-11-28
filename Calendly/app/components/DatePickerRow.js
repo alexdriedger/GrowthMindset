9b406b82c13be56b9e02d8903fc3bd68fa78c120
Mathew <mmacd97@gmail.com>
2017-11-28 00:16:45 -0800

    App/mathew/add date picker (#46)
    * Made the availability list look prettier
    
    * added picker row
    
    * Fixed onPress function
    
    * basic picker touch works
    
    * time picker function pops up now
    
    * got time picker working
    
    * got date picker working

---
+++ Calendly/app/components/DatePickerRow.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { TextInput, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import PropTypes from 'prop-types';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 8,
+    backgroundColor: 'green',
+  },
+  textBox: {
+    flex: 1,
+    height: 50,
+    padding: 8,
+    fontSize: 22,
+  },
+});
+
+class DatePickerRow extends Component {
+  render() {
+    return (
+      <TouchableOpacity onPress={() => this.props.onPress()}>
+        <View style={styles.container}>
+          <Icon name={this.props.icon} size={30} color="black" />
+          <Text
+            style={styles.textBox}
+            onChangeText={this.props.onChange}
+            placeholder={this.props.defaultText}
+          >
+            {this.props.text}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+DatePickerRow.propTypes = {
+  icon: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onPress: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+  defaultText: PropTypes.string.isRequired,
+};
+
+export default DatePickerRow;
